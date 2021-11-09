f42eb642efd05e110403c1b69c849755ee606894
Alexandre Mathias <mathias.alexandre@gmail.com>
2021-11-09 04:44:10 +0000

    added home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'screens/screens.dart';
+
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,8 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        
         primarySwatch: Colors.blue,
       ),
       home: const HomeScreen(),
@@ -21,13 +27,3 @@ class MyApp extends StatelessWidget {
 }
 
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Home'),),
-      
-    );
-  }
-}
